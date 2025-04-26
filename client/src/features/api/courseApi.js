b0245c25d19ada4f8051a5bee174bcafddbaca0c
Kuldip-14 <kuldipmistri14@gmail.com>
2025-04-26 23:16:34 +0530

    deploying

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -1,6 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_API = "https://bitacedemy.onrender.com/api/v1/course";
+const COURSE_API = window.location.hostname.includes("localhost")
+  ? "http://localhost:8080/api/v1/course"
+  : "https://bitacedemy.onrender.com/api/v1/course";
+
 export const courseApi = createApi({
   reducerPath: "courseApi",
   tagTypes: ["Refetch_Creator_Course", "Refetch_Lecture"],
