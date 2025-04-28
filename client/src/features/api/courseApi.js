26be908ea30d92c3359717fa048cd4f64d5ed2ce
Kuldip-14 <kuldipmistri14@gmail.com>
2025-04-28 22:58:20 +0530

    added localhost

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -1,8 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_API = window.location.hostname.includes("localhost")
-  ? "http://localhost:8080/api/v1/course"
-  : "https://bitacedemy.onrender.com/api/v1/course";
+const COURSE_API = "http://localhost:8080/api/v1/course";
 
 export const courseApi = createApi({
   reducerPath: "courseApi",
